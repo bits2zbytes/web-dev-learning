d71e6d2261dcfd31391619f549eeffaac901b47d
MONIKA <jhamonika711@gmail.com>
2021-12-15 21:32:07 +0530

    Simon

--- introToNode/index.js
+++
@@ -1,9 +0,0 @@
-//jshint esversion:6
-
-var superheroes = require("superheroes");
-var mySuperheroes = superheroes.random();
-console.log(mySuperheroes);
-
-var supervillains = require("supervillains");
-var aVillain = supervillains.random();
-console.log(aVillain);
--- jQuery/index.js
+++
@@ -1,5 +0,0 @@
-$(document).keypress(function(event) {
-  alert("Working");
-  var keyPressed = event.key;
-  $("h1").text(event.key);
-});
